0793514878cee9b6e023b167d6d6395274ab1552
Ashley Ong <ashleyong10@yahoo.com>
2025-03-18 03:11:50 -0400

    edit nav

--- frontend/spinkler-app/src/components/nav-bar.jsx
+++ frontend/spinkler-app/src/components/nav-bar.jsx
@@ -1,76 +1,39 @@
 import SDVLogo from '../images/SDVLogo.png';
+import React, {useState} from 'react';
 
 export default function NavBar() {
-  return (
-    <div style = {
-        {
-            display: 'flex',
-            flexDirection: 'row',
-            backgroundColor: 'red',
-        }
-    }>
+    const [isHidden, setIsHidden] = useState(true);
 
+  return (
     <header style = {
         {
             display: 'flex',
             flexDirection: 'row',
             backgroundColor: 'green',
+            width: '100vw',
+            justifyContent: 'space-between',
+            position: 'relative', /* Ensures dropdown is positioned relative to header */
         }
     }>
-        <a href="/" title="Home" className="title">
-            <picture>
-                <img src={SDVLogo} className="sdv-logo" alt="" />
-            </picture>
-        Stardew Valley Sprinkler Placement</a>
-        <nav>
-            <menu>
-                <button className='farm-type-button'>Farm Type</button>
-                <div className = "dropdown">
-                    <li>
-                        <button>
-                            <a href="/Standard">Standard Farm</a>
-                        </button>
-                    </li>
-                    <li>
-                        <button>
-                            <a href="/Riverland">Riverland Farm</a>
-                        </button>
-                    </li>
-                    <li>
-                        <button>
-                            <a href="/Forest">Forest Farm</a>
-                        </button>
-                    </li>
-                    <li>
-                        <button>
-                            <a href="/Hill-top">Hill-top Farm</a>
-                        </button>
-                    </li>
-                    <li>
-                        <button>
-                            <a href="/Wilderness">Wilderness Farm</a>
-                        </button>
-                    </li>
-                    <li>
-                        <button>
-                            <a href="/Four-Corners">Four Corners Farm</a>
-                        </button>
-                    </li>
-                    <li>
-                        <button>
-                            <a href="/Beach">Beach Farm</a>
-                        </button>
-                    </li>
-                    <li>
-                        <button>
-                            <a href="/Meadowlands">Meadowlands Farm</a>
-                        </button>
-                    </li>
-                </div>
-            </menu>
-        </nav>
-    </header>
 
-    </div>
+        <a href="/" title="Home" className="title">
+            <picture><img src={SDVLogo} className="sdv-logo" alt="" /></picture>
+        Stardew Valley Sprinkler Placement</a>
+        <div className="ham-menu" style={ {width: 'auto',} }>
+            <button id='ham-button' className='farm-type-button' onClick={() => setIsHidden(!isHidden)}>Farm Type</button>
+            <nav className={isHidden ? 'hidden' : 'nav-visible'}>
+                <ul className="dropdown">
+                    <li> <button><a href="/Standard">Standard Farm</a></button> </li>
+                    <li> <button><a href="/Riverland">Riverland Farm</a></button> </li>
+                    <li> <button><a href="/Forest">Forest Farm</a></button> </li>
+                    <li> <button><a href="/Hill-top">Hill-top Farm</a></button> </li>
+                    <li> <button><a href="/Wilderness">Wilderness Farm</a></button> </li>
+                    <li> <button><a href="/Four-Corners">Four Corners Farm</a></button> </li>
+                    <li> <button><a href="/Beach">Beach Farm</a></button> </li>
+                    <li> <button><a href="/Meadowlands">Meadowlands Farm</a></button> </li>
+                </ul>
+            </nav>
+        </div>
+    </header>
   );
 }
