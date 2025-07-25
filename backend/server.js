96b75459934e8cd6fdc5e5db196c695572983c57
RenrakDivad <vieclipsekd@gmail.com>
2025-07-25 18:53:22 -0500

    trying to connect backend

--- backend/server.js
+++ backend/server.js
@@ -38,5 +38,5 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
     .then(() => console.log('MongoDB connected'))
     .catch(err => console.log('MongoDB connection error:', err));
 
-const PORT = process.env.PORT || 4000; 
+const PORT = process.env.PORT || 3000; 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
